730fede4093d88be9d36ba77b7e2bc50bc5ace74
MCModNoob <ivannnnnn321@gmail.com>
2025-06-27 01:42:45 +0100

    flexbox center with basic styles

---
+++ front-end-practice/app/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+// import QrCodeComponent from "@/component/QrCodeComponent"; 
+
+export default function Home() {
+  return (
+    <div className="bg-blue-400 flex min-h-screen justify-center items-center px-4 py-8">
+
+      <div className="text-black w-fit border-2  bg-white font-semibold rounded p-2 sm:p-8 sm:flex-wrap">
+      
+        <div className=" sm:flex-1 md:px-4 md:py-4">
+
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+          Improve your 
+        </div>
+      </div>
+    </div>
+  )
+}
